b3390f674a301c962796cfa790ae011b439f6323
ben-basten <45583362+ben-basten@users.noreply.github.com>
2025-04-06 08:00:21 -0400

    fix(web): reset search suggestions after logout

--- web/src/lib/stores/search.store.ts
+++ web/src/lib/stores/search.store.ts
@@ -4,3 +4,9 @@ import { writable } from 'svelte/store';
 export const savedSearchTerms = persisted<string[]>('search-terms', [], {});
 export const isSearchEnabled = writable<boolean>(false);
 export const preventRaceConditionSearchBar = writable<boolean>(false);
+
+export const resetSearch = () => {
+  savedSearchTerms.set([]);
+  isSearchEnabled.set(false);
+  preventRaceConditionSearchBar.set(false);
+};
